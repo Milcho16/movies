ee4e10fb4bf9f16f0b4e295a468329e0595b0e69
Milcho16 <milchomanov7@gmail.com>
2024-07-09 14:04:06 +0300

    edit button on javascript

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-<button onclick="location.reload()">Рефреш</button>
+console.log
+("Hello");
+
+
+let is_clicked = false;
+ 
+function myFunction() {
+    let clickButtonElement = document.getElementById('click-button')
+    let buttonsDivElement = document.getElementById('buttons')
+ 
+    if (!is_clicked) {
+        let newButtonElement = document.createElement('button')
+        newButtonElement.classList.add('click-buttons')
+        newButtonElement.textContent = 'Click Me'
+ 
+        newButtonElement.addEventListener('click', () => {
+            is_clicked = false;
+            newButtonElement.remove()
+        })
+ 
+        buttonsDivElement.appendChild(newButtonElement)
+        is_clicked = true;
+    }
+  }
+;
+ 
